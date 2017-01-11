fc3ff5cb27e17c5bbbe8077d48d8dc277eab2821
Edward George <edwardgeorge@gmail.com>
2017-01-11 12:55:19 +0000

    tests: add quick test for sum operations

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,3 +1,5 @@
+import pytest
+
 from nstack import build
 from helper_defs import *
 
@@ -19,3 +21,21 @@ def test_process_schema():
     # slightly brittle, but will do for now...
     assert x.method1(({'fst': 3, 'snd': 4}, (1, 2, 5))) == "(Record(fst='fst', snd='snd'), (1, 2, 5))"
     assert x.method1(None) == "None"
+
+def test_sums():
+    a, b = build.process_schema(example)
+    x = a['Either'].new((1, 3))
+    assert isinstance(x, a['Either'])
+    assert isinstance(x, a['Either'].Right)
+    assert not isinstance(x, a['Either'].Left)
+    assert x.valueWithIndex() == (1, 3)
+    assert x.getRight() == 3
+    with pytest.raises(TypeError):
+        x.getLeft()
+    assert x.match((lambda a: False), (lambda a: True))
+    assert not x.match((lambda a: True), (lambda a: False))
+    assert x.match((lambda i: i), (lambda i: i * 3)) == 9
+    assert x.value == 3
+
+    assert isinstance(a['Either'].Left(3), a['Either'].Left)
+    assert not isinstance(a['Either'].Left(3), a['Either'].Right)
