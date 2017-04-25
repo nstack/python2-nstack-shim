5ef2c49e8aa6eab3e88c598cb1362d5f225eb20a
Edward George <edwardgeorge@gmail.com>
2017-04-25 12:43:53 +0100

    feat/cross-module-ns

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -16,14 +16,14 @@ def _object_with_method(method, name=None, cls_name=None):
 
 
 def test_process_schema():
-    a, b = build.process_schema(example)
+    a, b, c = build.process_schema(example)
     x = b(_object_with_method((lambda self, i: repr(i)), name='method1'))
     # slightly brittle, but will do for now...
     assert x.method1(({'fst': 3, 'snd': 4}, (1, 2, 5))) == "(Record(fst='fst', snd='snd'), (1, 2, 5))"
     assert x.method1(None) == "None"
 
 def test_sums():
-    a, b = build.process_schema(example)
+    a, b, c = build.process_schema(example)
     x = a['Either'].new((1, 3))
     assert isinstance(x, a['Either'])
     assert isinstance(x, a['Either'].Right)
@@ -44,27 +44,27 @@ def test_sums():
     assert not isinstance(a['Either'].Left(3), a['Either'].Right)
 
 def test_optional():
-    a, b = build.process_schema(example)
+    a, b, c = build.process_schema(example)
     assert a['Optional'](None) is None
     assert a['Optional'](3) == 3
     with pytest.raises(ValueError):
         a['Optional']("string")
 
 def test_nested_sum():
-    a, b = build.process_schema({'types': [['Foo', t_sum(('A', t_int), ('B', t_bool))], ],
-                                 'signatures': {'method1': [
-                                     t_tuple(t_int, t_record(('a', t_bool), ('b', t_ref('Foo')))),
-                                     t_tuple(t_ref('Foo'))]}})
+    a, b, c = build.process_schema({'types': [['Foo', t_sum(('A', t_int), ('B', t_bool))], ],
+                                    'signatures': {'method1': [
+                                        t_tuple(t_int, t_record(('a', t_bool), ('b', t_ref('Foo')))),
+                                        t_tuple(t_ref('Foo'))]}})
     x = b(_object_with_method((lambda self, i: i[1][1]), name='method1'))
     assert x.method1((1, (2, (1, False)))) == (1, False)
     assert x.method1((1, (2, (0, 3)))) == (0, 3)
 
 
 def test_sum_args():
-    a, b = build.process_schema({'types': [['Foo', t_sum(('A', t_sum(('C', t_int), ('D', t_bool))),
-                                                         ('B', t_tuple(t_int, t_bool)))], ],
-                                 'signatures': {
-                                     'method1': [t_ref('Foo'), t_tuple(optional(t_int), optional(t_bool))]}})
+    a, b, c = build.process_schema({'types': [['Foo', t_sum(('A', t_sum(('C', t_int), ('D', t_bool))),
+                                                            ('B', t_tuple(t_int, t_bool)))], ],
+                                    'signatures': {
+                                        'method1': [t_ref('Foo'), t_tuple(optional(t_int), optional(t_bool))]}})
     def method1(self, inp):
         return inp.match(lambda i: i.match(lambda j: (j, None),
                                            lambda j: (None, j)),
@@ -76,17 +76,17 @@ def test_sum_args():
     assert x.method1((1, (2, False))) == (2, False)
 
 def test_unit_input():
-    a, b = build.process_schema({'signatures': {'method1': [t_unit, t_bool]}})
+    a, b, c = build.process_schema({'signatures': {'method1': [t_unit, t_bool]}})
     x = b(_object_with_method((lambda self, i: i == ()), name='method1'))
     assert x.method1(()) is True
 
 def test_unit_output():
-    a, b = build.process_schema({'signatures': {'method1': [optional(t_unit), t_unit]}})
+    a, b, c = build.process_schema({'signatures': {'method1': [optional(t_unit), t_unit]}})
     x = b(_object_with_method((lambda self, i: ()), name='method1'))
     assert x.method1(None) == ()
 
 def test_lists():
-    a, b = build.process_schema({'signatures': {'method1': [t_array(t_int), t_int]}})
+    a, b, c = build.process_schema({'signatures': {'method1': [t_array(t_int), t_int]}})
     x = b(_object_with_method((lambda self, i: len(i)), name='method1'))
     assert x.method1([1,2,3,4,5]) == 5
 
